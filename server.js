8ec1fb090c2658ba7b55218326b07515456bed5d
jonathan1394 <33575621+jonathan1394@users.noreply.github.com>
2025-01-31 17:34:09 -0300

    Update server.js
    elimine la carpeta

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const port = 3000;
 // Configurar almacenamiento con Multer
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, 'uploads/');
+        cb(null, '/');
     },
     filename: (req, file, cb) => {
         cb(null, file.originalname);
