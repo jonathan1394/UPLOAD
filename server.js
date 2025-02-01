07e5b9e32832686c75d47cf3601f0f7fe26f638c
jonathan1394 <33575621+jonathan1394@users.noreply.github.com>
2025-02-01 12:03:11 -0300

    Sin paquetes

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const fs = require('fs');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 1000;
 
 // Asegurar que la carpeta '/tmp/uploads' existe
 const uploadDir = '/tmp/uploads';
