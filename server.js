afd86acdce5318308de5adfdedf6340cfd48c75f
jonathan1394 <33575621+jonathan1394@users.noreply.github.com>
2025-02-01 12:36:36 -0300

    Cambio puerto

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const fs = require('fs');
 
 const app = express();
-const port = 3000;
+const port = 10000;
 
 // Configurar almacenamiento con Multer
 const storage = multer.diskStorage({
